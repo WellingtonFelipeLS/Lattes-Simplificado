4e2ecadf98732325f6fb4059369ae840aaec6dbb
Wellington Felipe <wefeliso@gmail.com>
2023-04-09 09:43:52 -0300

    Protótipo da página do currículo

--- Lattes_Simplificado/settings.py
+++ Lattes_Simplificado/settings.py
@@ -124,3 +124,6 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
